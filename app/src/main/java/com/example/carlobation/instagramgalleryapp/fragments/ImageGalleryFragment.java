798d76cc937122a855a8dd1d95ab9e98875b1deb
Carlo Luis Bation <cbation@isbx.com>
2015-02-16 20:10:03 +0800

    Add and implement basic fragment for grid image gallery.

---
+++ app/src/main/java/com/example/carlobation/instagramgalleryapp/fragments/ImageGalleryFragment.java
@@ -0,0 +1,132 @@
+package com.example.carlobation.instagramgalleryapp.fragments;
+
+import android.support.v4.app.Fragment;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AbsListView;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.Gallery;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+
+import com.example.carlobation.instagramgalleryapp.R;
+import com.nostra13.universalimageloader.core.DisplayImageOptions;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.assist.FailReason;
+import com.nostra13.universalimageloader.core.listener.ImageLoadingProgressListener;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
+
+/**
+ * Created by carlobation on 2/16/15.
+ */
+public class ImageGalleryFragment extends Fragment {
+
+    private DisplayImageOptions options;
+
+    String[] imageUrls =  new String[]{};
+
+    protected AbsListView listView;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        options = new DisplayImageOptions.Builder()
+                .cacheInMemory(true)
+                .cacheOnDisk(true)
+                .considerExifParams(true)
+                .bitmapConfig(Bitmap.Config.RGB_565)
+                .build();
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_gallery_grid, container, false);
+        listView = (GridView) rootView.findViewById(R.id.grid);
+        ((GridView) listView).setAdapter(new ImageAdapter());
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
+        return rootView;
+    }
+
+
+    public class ImageAdapter extends BaseAdapter {
+
+        private LayoutInflater inflater;
+
+        ImageAdapter() {
+            inflater = LayoutInflater.from(getActivity());
+        }
+
+        @Override
+        public int getCount() {
+            return imageUrls.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public View getView(int position, View convertView, ViewGroup parent) {
+            final ViewHolder holder;
+            View view = convertView;
+            if (view == null) {
+                view = inflater.inflate(R.layout.gallery_image, parent, false);
+                holder = new ViewHolder();
+                assert view != null;
+                holder.imageView = (ImageView) view.findViewById(R.id.image);
+                holder.progressBar = (ProgressBar) view.findViewById(R.id.progress);
+                view.setTag(holder);
+            } else {
+                holder = (ViewHolder) view.getTag();
+            }
+
+            ImageLoader.getInstance()
+                    .displayImage(imageUrls[position], holder.imageView, options, new SimpleImageLoadingListener() {
+                        @Override
+                        public void onLoadingStarted(String imageUri, View view) {
+                            holder.progressBar.setProgress(0);
+                            holder.progressBar.setVisibility(View.VISIBLE);
+                        }
+
+                        @Override
+                        public void onLoadingFailed(String imageUri, View view, FailReason failReason) {
+                            holder.progressBar.setVisibility(View.GONE);
+                        }
+
+                        @Override
+                        public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                            holder.progressBar.setVisibility(View.GONE);
+                        }
+                    }, new ImageLoadingProgressListener() {
+                        @Override
+                        public void onProgressUpdate(String imageUri, View view, int current, int total) {
+                            holder.progressBar.setProgress(Math.round(100.0f * current / total));
+                        }
+                    });
+
+            return view;
+        }
+    }
+
+    static class ViewHolder {
+        ImageView imageView;
+        ProgressBar progressBar;
+    }
+}
