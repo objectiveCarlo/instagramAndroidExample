0164364eeb00d9c534d83bea0c797e6003cc4c90
Carlo Luis Bation <cbation@isbx.com>
2015-02-16 20:38:13 +0800

    Add custom application for instagram image gallery.

---
+++ app/src/main/java/com/example/carlobation/instagramgalleryapp/InstagramImageGalleryApplication.java
@@ -0,0 +1,34 @@
+package com.example.carlobation.instagramgalleryapp;
+
+import android.app.Application;
+import android.content.Context;
+
+import com.nostra13.universalimageloader.cache.disc.naming.Md5FileNameGenerator;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.assist.QueueProcessingType;
+
+/**
+ * Created by carlobation on 2/16/15.
+ */
+public class InstagramImageGalleryApplication extends Application {
+
+    @Override
+    public void onCreate() {
+
+        super.onCreate();
+
+        initImageLoader(getApplicationContext());
+    }
+
+    public static void initImageLoader(Context context) {
+
+        ImageLoaderConfiguration config = new ImageLoaderConfiguration.Builder(context)
+                .threadPriority(Thread.NORM_PRIORITY - 2)
+                .denyCacheImageMultipleSizesInMemory()
+                .diskCacheFileNameGenerator(new Md5FileNameGenerator())
+                .tasksProcessingOrder(QueueProcessingType.LIFO)
+                .build();
+        ImageLoader.getInstance().init(config);
+    }
+}
